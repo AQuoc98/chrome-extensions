99396e4a1cc6b1d45cf1bd6e36da3d09ac2b29e2
AQuoc98 <quocnguyen.developer@gmail.com>
2025-12-05 16:18:53 +0700

    chore: update dependencies and restructure Vite configuration for sst-blue-0001 package
    - Upgraded @crxjs/vite-plugin to version 2.2.1 and other related dependencies.
    - Added new package sst-blue-0001 with initial package.json.
    - Replaced base Vite configuration with a specific configuration for sst-blue-0001.
    - Removed the obsolete vite.config.base.js file.

--- packages/sst-blue-0001/vite.config.js
+++ packages/sst-blue-0001/vite.config.js
@@ -1,8 +1,27 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest.js'
-import { createBaseConfig } from '../../vite.config.base.js'
 
-// https://vitejs.dev/config/
-export default createBaseConfig({
-  plugins: [crx({ manifest })],
-})
+export default defineConfig(() => ({
+  root: import.meta.dirname,
+  cacheDir: '../../node_modules/.vite/packages/sst-blue-0001',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [react(), crx({ manifest })],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}))
